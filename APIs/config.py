5a02c3b94e5c342621558ea03198d08fa0f8e3ec
an-nguyen6 <atnguyen65@gmail.com>
2023-01-05 14:35:29 -0600

    removed api keys

--- APIs/config.py
+++ APIs/config.py
@@ -1,17 +1,17 @@
 import base64
 
 # FoodData
-fd_api_key = 'JOS7wARkFRFc9bk9d94bEqpg8hPR7vPSM3Lk123l'
+fd_api_key = [YOUR FOOD DATA API KEY]
 
 # Kroger
-kroger_client_id = 'intermediatepythonproject-32fd2ef3198aa79125db1f467993a7d42156649754783938330'
-kroger_client_secret = 'ctdQmwXXumi4CHSFsV0ZjUejV9FGnEXpcO4IhtwW'
+kroger_client_id = [YOUR KROGER CLIENT ID]
+kroger_client_secret = [YOUR KROGER CLIENT SECRET]
 kroger_encoded_client_info = base64.b64encode(f"{kroger_client_id}"
                                               f":{kroger_client_secret}".encode(
     'ascii')).decode('ascii')
 
 # Spoonacular
 # sp_api_key = '&apiKey=c3ca2072fe394799a2e5e6cd19c87fd6'
-sp_api_key = '72efea0b89msh2c33c33410c53c7p125632jsne38436fd9c93'
+sp_api_key = [YOUR SPOONACULAR API KEY]
 sp_host = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 
